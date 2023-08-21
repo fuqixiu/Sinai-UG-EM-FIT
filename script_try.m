ca6f7fd2f8d1de2db167ae6abe6af569d81eb90a
QiNan <53305147+fuqixiu@users.noreply.github.com>
2023-08-21 14:47:12 +0800

    add draft script note

---
+++ script_try.m
@@ -0,0 +1,7 @@
+IC_Choice= IC_Choice(:, 1:5);
+IC_offer = IC_offer(:, 1:5);
+IC_reward = IC_reward(:, 1:5);
+IC_pc = IC_pc(1:5,:);
+IC_pc_rt = IC_pc_rt(1:5,:);
+IC_RT = IC_RT(:,1:5);
+ID = ID(1:5,:);
